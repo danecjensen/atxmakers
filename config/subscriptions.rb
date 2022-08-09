400b408f68bda779f2e631033894f302cc910cc2
Dane Jensen <dane@sotmclub.com>
2022-08-08 21:04:30 -0500

    Update subscriptions.rb

--- config/subscriptions.rb
+++ config/subscriptions.rb
@@ -4,7 +4,7 @@ def stripe_price_id(subscription)
     credentials.dig(:stripe, :price_ids, subscription) ||
       "#{subscription}_dummy_stripe_price_id"
   else
-    credentials.stripe[:price_ids][subscription]
+#     credentials.stripe[:price_ids][subscription]
   end
 end
 
@@ -14,6 +14,6 @@ def revenue_cat_product_identifier(subscription)
     credentials.dig(:revenue_cat, :product_identifiers, subscription) ||
       "#{subscription}_dummy_revenue_cat_product_identifier"
   else
-    credentials.revenue_cat[:product_identifiers][subscription]
+#     credentials.revenue_cat[:product_identifiers][subscription]
   end
 end
